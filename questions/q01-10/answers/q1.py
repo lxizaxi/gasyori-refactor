ccab4b384a53d9b91d0f024e7d6baca8b5245441
lxizaxi <USERNAME@users.noreply.github.com>
2023-11-29 22:11:46 +0900

    ans: q1 and env

---
+++ questions/q01-10/answers/q1.py
@@ -0,0 +1,22 @@
+# %%
+# ------------------------ #
+import sys
+
+# import project root
+sys.path.append("../../../")
+# ------------------------ #
+
+from pathlib import Path
+
+import cv2
+from cv2.typing import MatLike
+
+from utils.image_util import ImageUtil
+
+imori_path: Path = ImageUtil.get_asset_path("imori.jpg")
+original_img: MatLike = cv2.imread(str(imori_path))
+
+converted_img: MatLike = cv2.cvtColor(original_img, cv2.COLOR_RGB2BGR)
+ImageUtil.show_image_cv2(converted_img, title="Original Image")
+
+# %%
---
+++ utils/image_util.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+import cv2
+from cv2.typing import MatLike
+from matplotlib import pyplot as plt
+
+
+@dataclass
+class ImageUtil:
+    @staticmethod
+    def show_image_cv2(img: MatLike, title: str = "Image") -> None:
+        rgb_ima: MatLike = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        plt.imshow(rgb_ima)
+        plt.title(title)
+        plt.axis("off")
+        plt.show()
+
+    @staticmethod
+    def get_asset_path(asset_name: str) -> Path:
+        assets_path: Path = Path(__file__).parent.parent / "assets"
+        paths: list[Path] = [p for p in assets_path.iterdir() if p.name == asset_name]
+
+        if len(paths) != 1:
+            raise Exception(f"Asset {asset_name} not specified")
+        return paths[0]
