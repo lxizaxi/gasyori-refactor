df3dcafaa5dc44a4e51a6622f84f4d83cb4c1655
lxizaxi <USERNAME@users.noreply.github.com>
2023-11-30 22:35:48 +0900

    ans: q4

---
+++ questions/q01-10/answers/q4.py
@@ -0,0 +1,22 @@
+# %%
+# ------------------------ #
+import sys
+
+# import project root
+sys.path.append("../../../")
+# ------------------------ #
+
+
+import cv2
+from cv2.typing import MatLike
+
+from utils.image_util import ImageUtil
+
+original_img: MatLike = ImageUtil.get_asset("imori.jpg")
+converted_img: MatLike = cv2.cvtColor(original_img, cv2.COLOR_RGB2GRAY)
+
+
+_, binary_image = cv2.threshold(converted_img, 0, 255, cv2.THRESH_OTSU)
+ImageUtil.show_image_cv2(binary_image, title="Original Image")
+
+# %%
